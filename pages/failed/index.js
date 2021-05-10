6eef8c30d34c92ab2a9a9146c30233e30c282f58
dev Moody <developer.mody@gmail.com>
2021-05-10 13:56:28 +0200

    ADD failed payment  page

---
+++ pages/failed/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import FailedPayment from '../../modules/FailedPayment'
+
+const FaildPaymentPage = () => <>
+    
+    <Head>
+        <title> Faild Payment  </title>
+    </Head>
+    <FailedPayment />
+</>
+
+export default FaildPaymentPage
