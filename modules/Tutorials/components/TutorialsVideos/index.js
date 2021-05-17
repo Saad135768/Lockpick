a30d31a5416252c594a482f2b1a0a0a099fa1ad1
Saad135768 <60039644+Saad135768@users.noreply.github.com>
2021-05-17 14:17:40 +0200

    add search video

--- modules/Tutorials/components/TutorialsVideos/index.js
+++ modules/Tutorials/components/TutorialsVideos/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import useStyles from './style'
 import ReactPlayer from 'react-player/lazy'
+import { AiOutlineSearch } from 'react-icons/ai'
 import { useRouter } from 'next/router'
 import { CMS } from '../../../Home/Header/data'
 import { useQuery } from '@apollo/react-hooks'
@@ -14,7 +15,7 @@ import { pathOr } from 'ramda'
 function TabPanel(props) {
   const { children, value, index, ...other } = props
   const classes = useStyles()
- 
+
   return (
     <div
       role="tabpanel"
@@ -46,55 +47,101 @@ function a11yProps(index) {
 }
 
 export default function VerticalTabs() {
-
   const { data: cmsData } = useQuery(CMS)
 
-  const [value, setValue] = React.useState(0)
+  const [value, setValue] = useState(0)
+  const [searchTerm, setSearchTerm] = useState()
 
   const router = useRouter()
   const { asPath } = router
 
+  const items = pathOr([], ['cms', 'secondarySlider'], cmsData)
+
   useEffect(() => {
     //  This condition checks if the user navigates to this page through pressing on 'while download....'btn,
     //   the second video should be the default one
-  if(asPath.includes('download')) setValue(1)
+    if (asPath.includes('download')) setValue(1)
   }, [])
 
   const handleChange = (_, newValue) => {
     setValue(newValue)
   }
+
+  const search = (e) => items.find(({ name }, index) => {
+    const searched = name.toLowerCase().includes(e.toLowerCase())
+    if (searched) setValue(index)
+    return searched
+  })
+
   const classes = useStyles()
 
   return (
-    <div className={classes.root}>
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        className={classes.tabs}
-        indicatorColor="primary"
-        classes={{ indicator: classes.indicator }}
-      >
-          {pathOr([], ['cms', 'secondarySlider'], cmsData).map(({ name, url }, index) => (
-          <Tab label={name} {...a11yProps(index)} key={url}/>
-            ))}
-      </Tabs>
-      {pathOr([], ['cms', 'secondarySlider'], cmsData).map(({ name, url }, index) => (
-          <TabPanel className={classes.width} key={url} value={value} index={index}>
-                {name}
-                <ReactPlayer
-                  className="react-player"
-                  url={url}
-                  width="100%"
-                  height="330px"
-                  controls={true}
-                  playing={false}
-                  autoPlay={false}
-                />
-          </TabPanel>
-      ))}
-    </div>
+    <>
+      <div className={classes.search}>
+        <input
+          type="text"
+          className="searchTerm"
+          placeholder="Search for videos ..."
+          onChange={e => {
+            setSearchTerm(e.target.value)
+          }}
+          onKeyPress={e => {
+            if (e.key === 'Enter') {
+              search(searchTerm)
+              // setSearchTerm()
+            }
+          }}
+        />
+        <button
+          onClick={() => {
+            search(searchTerm)
+            // setSearchTerm()
+          }}
+          type="button"
+          className="searchButton"
+        >
+          <AiOutlineSearch />
+        </button>
+      </div>
+      <div className={classes.root}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+          indicatorColor="primary"
+          classes={{ indicator: classes.indicator }}
+        >
+          {items.map(
+            ({ name, url }, index) => (
+              <Tab label={name} {...a11yProps(index)} key={url} />
+            ),
+          )}
+        </Tabs>
+        {items.map(
+          ({ name, url }, index) => (
+            <TabPanel
+              className={classes.width}
+              key={url}
+              value={value}
+              index={index}
+            >
+              {name}
+              <ReactPlayer
+                className="react-player"
+                url={url}
+                width="100%"
+                height="330px"
+                controls={true}
+                playing={false}
+                autoPlay={false}
+              />
+            </TabPanel>
+          ),
+        )}
+      </div>
+    </>
   )
 }
--- modules/Tutorials/components/TutorialsVideos/style.js
+++ modules/Tutorials/components/TutorialsVideos/style.js
@@ -1,12 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(() => ({
-
   root: {
-    
     flexGrow: 1,
     backgroundColor: '#2d4562',
-    color:'#fff',
+    color: '#fff',
     height: 420,
     display: 'flex',
     '@media (max-width:767px)': {
@@ -14,50 +12,47 @@ const useStyles = makeStyles(() => ({
       height: '700px',
 
       ' &  .MuiTabs-scrollButtonsDesktop ': {
-       display:'block',
-      }, 
+        display: 'block',
+      },
       ' &  .MuiTabScrollButton-vertical ': {
-        textAlign:'center',
-       }, 
-       ' &  .MuiTab-root': {
-        width:'100%',
-        maxWidth:'100%',
-       }, 
-      
-      
+        textAlign: 'center',
+      },
+      ' &  .MuiTab-root': {
+        width: '100%',
+        maxWidth: '100%',
+      },
     },
 
     ' & .react-player__preview': {
       height: '330px ​!important',
-      },
+    },
     ' & svg.MuiSvgIcon-root.MuiSvgIcon-fontSizeSmall': {
       fontSize: 35,
-      },
+    },
     ' & button': {
-      boxShadow:'none',
-      fontSize:'16px',
+      boxShadow: 'none',
+      fontSize: '16px',
       background: '#3780d5',
       borderRdius: 0,
       marginBottom: 5,
       fontFamily: 'Poppins-SemiBold',
-      width:'100%',
-      maxWidth:'100%',
-      padding:'10px 4px',
+      width: '100%',
+      maxWidth: '100%',
+      padding: '10px 4px',
 
-      color:'#fff',
+      color: '#fff',
+    },
+
+    ' & p': {
+      padding: 0,
+      marginBottom: 35,
+      '@media (max-width:767px)': {
+        fontSize: 18,
       },
-      
-      ' & p': {
-        padding:0,
-        marginBottom: 35,
-        '@media (max-width:767px)': {
-fontSize:18,
-        },
-        },
-        ' & .react-player': {
-          marginTop: 20,
-          },
-       
+    },
+    ' & .react-player': {
+      marginTop: 20,
+    },
   },
   tabs: {
     display: 'flex',
@@ -75,19 +70,45 @@ fontSize:18,
       padding: 0,
     },
     '& a.ytp-impression-link ': {
-      display:'none',
-     }, 
-  },
-  playicon:{
-    width:'60px !important',
-  },
-  width:{
-    width:'65% !important',
-    '@media (max-width:767px)': {
-      width:'100% !important',
-
+      display: 'none',
     },
   },
+  playicon: {
+    width: '60px !important',
+  },
+  width: {
+    width: '65% !important',
+    '@media (max-width:767px)': {
+      width: '100% !important',
+    },
+  },
+  search: {
+    display: 'flex',
+    '& .searchButton': {
+      color: '#fff',
+    border: '1px solid #346EB3',
+    cursor: 'pointer',
+    height: 48,
+    fontSize: 20,
+    background: '#346EB3',
+    textAlign: 'center',
+    fontFamily: 'Poppins-Light',
+    borderRadius: '0 12px 12px 0',
+    },
+    '& .searchTerm': {
+      color: '#fff',
+      border: 0,
+      height: 48,
+      outline: 'none',
+      padding: 5,
+      fontSize: 15,
+      background: '#4b5059',
+      fontFamily: 'Poppins-Light',
+      borderRight: 'none',
+      paddingLeft: 30,
+      borderRadius: '12px 0 0 12px',
+    }
+  }
 }))
 
 export default useStyles
